c83f136f578f9c13a5467c728375d9403a0fa2db
Harry MacCode <emarh.harry.code@gmail.com>
2024-10-23 20:36:04 +0200

    Cross-env and script updated

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const app = require('./app');
 dotenv.config();
 
 const port = process.env.PORT || 8081;
+console.log(process.env.NODE_ENV);
 
 app.listen(port, () => console.log(`Server is running at ${port}`));
 
