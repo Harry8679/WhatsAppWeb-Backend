910759371493c10b42d4369aa16e281e0bbbc653
Harry MacCode <emarh.harry.code@gmail.com>
2024-10-23 22:00:52 +0200

    All middlewares added

--- src/app.js
+++ src/app.js
@@ -2,7 +2,14 @@ const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize')
+const cookieParser = require('cookie-parser');
+const compression = require('compression');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+
 dotenv.config();
+
 const app = express();
 
 // Morgan
@@ -12,6 +19,29 @@ if (process.env.NODE_ENV !== 'production') {
 
 // Helmet
 app.use(helmet());
+
+// Parse json request body
+app.use(express.json());
+
+// Parse json request body
+app.use(express.urlencoded({ extended: true }));
+
+// Sanitize request data
+app.use(mongoSanitize());
+
+// Enable cookie parser
+app.use(cookieParser());
+
+// gzip compression
+app.use(compression());
+
+// file upload
+app.use(fileUpload({
+    useTempFiles: true,
+}));
+
+// cors
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello from Server API');
