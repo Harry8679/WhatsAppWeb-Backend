338e1db959e6a3d5d7ebbe0d3781db2755683411
Harry MacCode <emarh.harry.code@gmail.com>
2024-10-24 05:34:02 +0200

    Logger added in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const dotenv = require('dotenv');
 const app = require('./app');
+const logger = require('./config/logger.config');
 
 dotenv.config();
 
 const port = process.env.PORT || 8081;
 console.log(process.env.NODE_ENV);
 
-app.listen(port, () => console.log(`Server is running at ${port}`));
+// app.listen(port, () => console.log(`Server is running at ${port}`));
+app.listen(port, () => logger.info(`Server is running at ${port}`));
 
