3b1dc509bec0ef4ccbef78a863bffd2ebf7ade66
Harry MacCode <emarh.harry.code@gmail.com>
2024-10-27 23:31:50 +0100

    User Model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
-const userSchela = mongoose.Schema({
+const userSchema = mongoose.Schema({
     name: {
         type: String,
         required: [true, 'Please provide your name']
@@ -8,6 +9,28 @@ const userSchela = mongoose.Schema({
     email: {
         type: String,
         required: [true, 'Please provide your email address'],
-        unique: [true, 'This email address already exist']
+        unique: [true, 'This email address already exist'],
+        lowercase: true,
+        validate: [validator.isEmail, 'Please enter a valid email address']
+    },
+    picture: {
+        type: String,
+        default: 'https://as1.ftcdn.net/v2/jpg/03/46/83/96/1000_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg'
+    },
+    status: {
+        type: String,
+        default: 'Hey there ! I am using whatsapp'
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide your password'],
+        minLength: [6, 'Please make sure you have at least 6 characters'],
+        maxLength: [128, 'Please make sure your password is less than 128 characters long']
     }
+}, {
+    collection: 'user',
+    timestamps: true
 });
+
+const UserModel = mongoose.models.UserModel || mongoose.model('UserModel', userSchema);
+module.exports = UserModel;
