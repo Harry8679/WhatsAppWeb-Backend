778dc509ceb6581dc92ede6205746c41a6a1375b
Harry MacCode <emarh.harry.code@gmail.com>
2024-10-28 21:59:43 +0100

    access token, refresh token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,12 +1,15 @@
-// const { createUser } = require("../services/auth.service");
-
 const createUser = require("../services/auth.service");
+const dotenv = require('dotenv');
+dotenv.config();
 
 const register = async (req, res, next) => {
     try {
         const { name, email, picture, status, password } = req.body;
         // console.log(name, email, picture, status, password);
         const newUser = await createUser({ name, email, picture, status, password });
+
+        const access_token = await generateToken({ userId: newUser._id}, '1d', process.env.ACCESS_TOKEN_SECRET);
+        const refresh_token = await generateToken({ userId: newUser._id}, '30d', process.env.REFRESH_TOKEN_SECRET);
 
         res.status(201).json(newUser);
     } catch (error) {
