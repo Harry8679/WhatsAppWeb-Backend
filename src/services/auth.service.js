9508f00a5aa1c61304c66c3b1c5cb3ab648cc945
Harry MacCode <emarh.harry.code@gmail.com>
2024-10-28 20:24:07 +0100

    Check status length

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,4 +1,5 @@
 const createHttpError  = require('http-errors');
+const validator = require('validator');
 
 const createUser = async(userData) => {
     const { name, email, picture, status, password } = userData;
@@ -6,6 +7,16 @@ const createUser = async(userData) => {
     // Check if fields are empty
     if (!name || !email || !password) {
         throw createHttpError.BadRequest('Please fill all fields');
+    }
+
+    // Check name length
+    if (!validator.isLength(name, { min:2, max: 16 })) {
+        throw createHttpError.BadRequest('Please make sure your name is between 2 and 16 characters long.');
+    }
+
+    // Check status length
+    if (status) {
+        throw createHttpError.BadRequest('Please make sure your status is less than 64 characters long.');
     }
 }
 
