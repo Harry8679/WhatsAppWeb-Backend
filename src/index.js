4f95ce580a2197eb06309c4742abed8715b9fa97
Harry MacCode <emarh.harry.code@gmail.com>
2024-11-01 17:29:05 +0100

    socket io instaled

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const dotenv = require('dotenv');
 const app = require('./app');
 const logger = require('./config/logger.config');
 const mongoose = require('mongoose');
+const http = require('http');
+const socketIo = require('socket.io');
 
 dotenv.config();
 
@@ -10,7 +12,7 @@ const { MONGODB_URI } = process.env;
 const port = process.env.PORT || 8081;
 console.log(process.env.NODE_ENV);
 
-// Exit on mongodb errors
+// Exit on MongoDB errors
 mongoose.connection.on('error', (err) => {
     logger.error(`MongoDB Connection error: ${err}`);
     process.exit(1);
@@ -19,35 +21,46 @@ mongoose.connection.on('error', (err) => {
 // MongoDB debug mode
 if (process.env.NODE_ENV !== 'production') {
     mongoose.set('debug', true);
-};
+}
 
-// mongodb connection
-mongoose.connect(MONGODB_URI, {}).then(() => logger.info('Connected to Mongo DB ...'));
+// MongoDB connection
+mongoose.connect(MONGODB_URI, {}).then(() => logger.info('Connected to MongoDB ...'));
 
-let server;
-// app.listen(port, () => console.log(`Server is running at ${port}`));
-server = app.listen(port, () => {
-    logger.info(`Server is running at ${port}`);
-    console.log('process id', process.pid);
+// Create HTTP server and initialize Socket.IO
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// Configure Socket.IO connection event
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    // You can add more event listeners here, e.g., for disconnect
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+// Start the server
+server.listen(port, () => {
+    logger.info(`Server is running on http://localhost:${port}`);
+    console.log('Process ID', process.pid);
 });
 
 // Handle server errors
 const exitHandler = () => {
-    if (server)  {
-        logger.info('Server closed.')
+    if (server) {
+        logger.info('Server closed.');
         process.exit(1);
     } else {
         process.exit(1);
     }
-}
+};
 
 const unexpectedErrorHandler = (error) => {
     logger.error(error);
-    process.exit(1);
     exitHandler();
-}
+};
 
-// SIGTERM
+// SIGTERM handling
 process.on('SIGTERM', () => {
     if (server) {
         logger.info('Server closed');
@@ -57,4 +70,3 @@ process.on('SIGTERM', () => {
 
 process.on('uncaughtException', unexpectedErrorHandler);
 process.on('unhandledRejection', unexpectedErrorHandler);
-
