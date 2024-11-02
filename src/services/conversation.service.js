093925782f707c71edccf9e06076d5d7cb5ab0fb
Harry MacCode <emarh.harry.code@gmail.com>
2024-11-02 07:58:52 +0100

    Conversation service updated

--- src/services/conversation.service.js
+++ src/services/conversation.service.js
@@ -1,3 +1,13 @@
+import ConversationModel from "../models/conversation.model";
+
 export const doesConversationExist = async (sender_id, receiver_id) => {
-    
+    let convo = await ConversationModel.find({
+        isGroup: false,
+        $and: [
+            { users: { $elemMatch: { $req: sender_id } } },
+            { users: { $elemMatch: { $req: receiver_id } } },
+        ],
+    })
+    .populate('users', '-passwords')
+    .populate('latestMessage');
 };
