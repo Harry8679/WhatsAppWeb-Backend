4d8e833a6cdf3fea8efd584551498d5dca18758c
Harry MacCode <emarh.harry.code@gmail.com>
2024-11-02 07:42:32 +0100

    Conversation update 1

--- src/controllers/conversation.controller.js
+++ src/controllers/conversation.controller.js
@@ -1,3 +1,19 @@
-const create_open_conversation = async (req, res) => {}
+const createHttpError = require('http-errors');
+const logger = require('../config/logger.config');
+
+const create_open_conversation = async (req, res, next) => {
+    try {
+        const sender_id = req.user.userId;
+        const { receiver_id } = req.body;
+        // Check if receiver_id is provided
+        if (!receiver_id) {
+            logger.error('Please provide the user id you wanna start a conversation with !');
+            throw createHttpError.BadRequest('Something went wrong');
+        }
+        res.send('Conversation created');
+    } catch(error) {
+        next(error);
+    };
+};
 
 module.exports = { create_open_conversation };
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,19 +1,20 @@
 const createHttpError = require('http-errors');
 const jwt = require('jsonwebtoken');
 
-export default async function (req, res, next) {
-    if(!req.hseader['authorization']) {
+module.exports = async function (req, res, next) {  // Remplace `export default` par `module.exports =`
+    if (!req.headers['authorization']) {
         return next(createHttpError.Unauthorized());
     }
     const bearerToken = req.headers['authorization'];
     const token = bearerToken.split(' ')[1];
+    
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
         if (err) {
             console.log(err.name);
             console.log(err.message);
-            return;
+            return next(createHttpError.Unauthorized());
         }
         req.user = payload;
         next();
     });
-}
+};
