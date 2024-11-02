a9809d93d08889c4215a9fc950fadc9a30176adf
Harry MacCode <emarh.harry.code@gmail.com>
2024-11-02 13:17:46 +0100

    Message Model

---
+++ src/models/message.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const { collection } = require('./user.model');
+const { ObjectId } = mongoose.Schema.Types;
+
+const messageSchema = mongoose.Schema({
+    sender: {
+        type: ObjectId,
+        ref: 'UserModel'
+    },
+    message: {
+        type: String,
+        trim: true
+    },
+    conversation: {
+        type: String,
+        ref: 'ConversationModel'
+    },
+    files: [],
+}, {
+    collection: 'messages',
+    timestamps: true
+});
+
+const MessageModel = mongoose.MessageModel || mongoose.model('MessageModel', messageSchema);
+
+module.exports = MessageModel;
