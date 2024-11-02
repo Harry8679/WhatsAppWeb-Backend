15954c262576da569223c9cb8a8dfbce0cf6c03c
Harry MacCode <emarh.harry.code@gmail.com>
2024-11-02 13:33:24 +0100

    Message Routes updated

--- src/routes/message.route.js
+++ src/routes/message.route.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const trimRequest = require('trim-request');
 const authMiddleware = require('../middlewares/auth.middleware');
+const { sendMessage, getMessage } = require('../controllers/message.controller');
 
 const router = express.Router();
 
-router.route('/').post(trimRequest.all, authMiddleware);
+router.route('/').post(trimRequest.all, authMiddleware, sendMessage);
+router.route('/').get(trimRequest.all, authMiddleware, getMessage);
