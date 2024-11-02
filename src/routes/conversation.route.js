1db21040031a552a49256cea00916febb1fa2bdd
Harry MacCode <emarh.harry.code@gmail.com>
2024-11-02 07:14:19 +0100

    Cinversation route

---
+++ src/routes/conversation.route.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const trimRequest = require('trim-request');
+const authMiddleware = require('../middlewares/auth.middleware');
+const { create_open_conversation } = require('../controllers/conversation.controller');
+
+const router = express.Router();
+
+router.route('/').post(trimRequest.all, authMiddleware, create_open_conversation);
+
+module.exports = router;
