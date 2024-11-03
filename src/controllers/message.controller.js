b028ebeab64d257b015daf27072720ee332b2938
Harry MacCode <emarh.harry.code@gmail.com>
2024-11-03 14:43:56 +0100

    Send Message

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -1,6 +1,42 @@
+const logger = require('../config/logger.config');
+// const { populate } = require('../models/user.model');
+const { createMessage, populatedMessage, updateLatestMessage } = require('../services/message.service');
+
 const sendMessage = async (req, res, next) => {
-    res.send('Send Message');
-}
+    try {
+        const user_id = req.user.userId;
+        const { message, convo_id, files } = req.body;
+
+        console.log('Sending message from user:', user_id);
+        console.log('Conversation ID:', convo_id);
+        console.log('Message:', message);
+
+        if (!convo_id || (!message && !files)) {
+            logger.error('Please provide a conversation id and a message body');
+            return res.status(400).json({ message: 'Missing data' });
+        }
+
+        const msgData = {
+            sender: user_id,
+            message,
+            conversation: convo_id,
+            files: files || []
+        };
+        let newMessage = await createMessage(msgData);
+        console.log('Message created:', newMessage);
+
+        let populatedMsg = await populatedMessage(newMessage._id);
+        console.log('Populated message:', populatedMsg);
+
+        await updateLatestMessage(convo_id, newMessage);
+        res.json(populatedMsg);
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+};
+
+
 
 const getMessage = async (req, res, next) => {
     res.send('Get Message');
---
+++ src/services/message.service.js
@@ -0,0 +1,62 @@
+const createHttpError = require('http-errors');
+const { MessageModel, ConversationModel } = require('../models/index');
+
+const createMessage = async (data) => {
+    let newMessage = await MessageModel.create(data);
+    if (!newMessage) {
+        throw createHttpError.BadRequest('Oops...Something went wrong !');
+    }
+    return newMessage;
+}
+
+const populatedMessage = async (id) => {
+    let msg = await MessageModel.findById(id)
+        .populate({
+            path: 'sender',
+            select: 'name picture',
+            model: 'UserModel'
+        })
+        .populate({
+            path: 'conversation',
+            select: 'name isGroup users',
+            model: 'ConversationModel',
+            populate: {
+                path: 'users',
+                select: 'name email picture status',
+                model: 'UserModel'
+            }
+        });
+    if (!msg) throw createHttpError.BadRequest('Oops...Something went wrong !');
+    return msg;
+}
+
+const updateLatestMessage = async (convo_id, msg) => {
+    try {
+        // Vérifiez que convo_id existe
+        const conversation = await ConversationModel.findById(convo_id);
+        if (!conversation) {
+            throw createHttpError.BadRequest(`Conversation with ID ${convo_id} not found`);
+        }
+        
+        // Mettez à jour la conversation avec latestMessage
+        const updatedConvo = await ConversationModel.findByIdAndUpdate(
+            convo_id,
+            { latestMessage: msg._id },
+            { new: true }
+        );
+
+        if (!updatedConvo) {
+            throw createHttpError.BadRequest(`Failed to update latestMessage for conversation ${convo_id}`);
+        }
+
+        console.log("Conversation successfully updated with latest message:", updatedConvo);
+        return updatedConvo;
+
+    } catch (error) {
+        console.error("Error in updateLatestMessage:", error);
+        throw createHttpError.BadRequest('Oops...Something went wrong in updateLatestMessage!');
+    }
+};
+
+
+module.exports = { createMessage, populatedMessage, updateLatestMessage };
