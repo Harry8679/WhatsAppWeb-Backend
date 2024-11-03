1d2775dd536d9d9edefdd82ffd71c02cf50871eb
Harry MacCode <emarh.harry.code@gmail.com>
2024-11-03 22:58:47 +0100

    Message Route updated

--- src/routes/message.route.js
+++ src/routes/message.route.js
@@ -6,6 +6,6 @@ const { sendMessage, getMessage } = require('../controllers/message.controller')
 const router = express.Router();
 
 router.route('/').post(trimRequest.all, authMiddleware, sendMessage);
-router.route('/').get(trimRequest.all, authMiddleware, getMessage);
+router.route('/:convo_id').get(trimRequest.all, authMiddleware, getMessage);
 
 module.exports = router;
