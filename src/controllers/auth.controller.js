9dd21f95417e9d5b491a6e4eade7a0a34cd8539a
Harry MacCode <emarh.harry.code@gmail.com>
2024-11-07 03:18:18 +0100

    access_token uncommented

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -54,7 +54,7 @@ const login = async (req, res, next) => {
 
         res.json({
             message: 'Login Success',
-            // access_token,
+            access_token,
             _id: user._id,
             name: user.name,
             email: user.email,
